6aa2ea343a0cd7a080f07e6465646c480da2100b
baixiaoji <1041874421@qq.com>
2017-11-05 17:38:49 +0800

    use the extract-text-webpack-plugin toextract the css to a independce file (使用loader将css文件抽离为一个独立的文件)

--- use-loader/webpack.config.js
+++ use-loader/webpack.config.js
@@ -1,26 +1,33 @@
 const path = require("path")
+const ExtractTextPlugin = require("extract-text-webpack-plugin")
 module.exports = {
-  entry:'./index.js',
-  output:{
-  path: path.resolve(__dirname,'dist'), // 输出的目录
-  filename: 'bundle.js' // 打包过后的文件名
+  entry: './index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'), // 输出的目录
+    filename: 'bundle.js' // 打包过后的文件名
   },
-module:{
-  rules:[
-    {
-      test:/\.js$/, // 匹配规则
-      use: 'babel-loader', // 使用那个loader处理文件
-      exclude:/node_module/ , //不包括的目录
-    },
-    {
-      test:/\.css$/,
-      use:['style-loader',{
-           loader:'css-loader',
-           options:{
-              modules:true
-           }
-      }]
-    }   
-]
-}
+  module: {
+    rules: [
+      {
+        test: /\.js$/, // 匹配规则
+        use: 'babel-loader', // 使用那个loader处理文件
+        exclude: /node_module/, //不包括的目录
+      },
+      {
+        test: /\.css$/,
+        loader: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          use: [{
+            loader: 'css-loader',
+            options: {
+              modules: true
+            }
+          }]
+        })
+      }
+    ]
+  },
+  plugins: [
+    new ExtractTextPlugin('css/[name].[hash].css')
+  ]
 }
