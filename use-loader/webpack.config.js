891fac5a4ae0c248b0789701bc8a34bc718ce7d0
baixiaoji <1041874421@qq.com>
2017-11-05 17:02:41 +0800

    use the babel loader and an example for using the loader in the webpack.config.js (使用了babel-loader,预设的配置可以写在.babelrc里面)

---
+++ use-loader/webpack.config.js
@@ -0,0 +1,17 @@
+const path = require("path")
+module.exports = {
+  entry:'./index.js',
+  output:{
+  path: path.resolve(__dirname,'dist'), // 输出的目录
+  filename: 'bundle.js' // 打包过后的文件名
+  },
+module:{
+  rules:[
+    {
+      test:/\.js$/, // 匹配规则
+      use: 'babel-loader', // 使用那个loader处理文件
+      exclude:/node_module/ , //不包括的目录
+    }
+]
+}
+}
