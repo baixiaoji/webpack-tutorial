d0186587cb61a4febfdc835b7d096d10abaddde6
baixiaoji <1041874421@qq.com>
2018-04-16 21:20:24 +0800

    [feat] add the css and less loader to change the file

--- src/app.js
+++ src/app.js
@@ -1,11 +1,15 @@
+import './css/app.css'
+import './css/app.less'
 let BD = {
     name: 'bigdream',
     member: ['zlj', 'zpj', 'zpk', 'jh', 'gxf', 'yj'],
     sayLeaderName(){
         this.member.map((name, i) => {
             if (i > 0) {
-                console.log(`${name} say: ' our leader is zlj '`)
+                console.log(`${name} say: 'our leader is zlj'`)
             }
         })
     }
 }
+
+BD.sayLeaderName();
--- webpack.config.js
+++ webpack.config.js
@@ -6,13 +6,21 @@ module.exports = {
     },
     output: {
         path: path.resolve(__dirname, './dist'),
-        filename: '[name]-[hash:5].js'
+        filename: '[name].js'
     },
     module: {
         rules: [
             {
                 test: /\.js$/,
                 use: 'babel-loader'
+            },
+            {
+                test:/\.css$/,
+                use:['style-loader','css-loader']
+            },
+            {
+                test: /\.less$/,
+                use: ['style-loader','css-loader','less-loader']
             }
         ]
     }
