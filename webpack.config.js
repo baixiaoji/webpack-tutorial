703496e1f5a90f8ac27a760914ebaa6afc4abff5
baixiaoji <1041874421@qq.com>
2018-04-16 18:38:30 +0800

    [feat] 增加了babel去转es6/7的代码

--- webpack.config.js
+++ webpack.config.js
@@ -7,5 +7,13 @@ module.exports = {
     output: {
         path: path.resolve(__dirname, './dist'),
         filename: '[name]-[hash:5].js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                use: 'babel-loader'
+            }
+        ]
     }
 }
